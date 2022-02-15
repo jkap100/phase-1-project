7905e2d514a241048a58683d09b21e9afae72e4e
jkap100 <95940562+jkap100@users.noreply.github.com>
2022-02-15 11:17:26 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,124 @@
+const form = document.querySelector("#searchForm");
+const movieDiv = document.querySelector("#movie-list");
+const clearFormBtn = document.querySelector("#clear-form");
+const commentDiv = document.querySelector("#comment-div");
+const likesDiv = document.querySelector("#likes-div");
+
+clearFormBtn.addEventListener("click", () => {
+  movieDiv.innerHTML = "";
+  commentDiv.innerHTML = "";
+  likesDiv.innerHTML = "";
+});
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
+  makeImages(res.data);
+
+  form.elements.query.value = "";
+});
+
+const makeImages = (shows) => {
+  // console.log(shows);
+  movieDiv.innerHTML = "";
+  for (let result of shows) {
+    if (result.show.image) {
+      const movieSpan = document.createElement("span");
+      const newImg = document.createElement("img");
+      const movieTitle = document.createElement("h4");
+      const movieRating = document.createElement("h5");
+      let originalImg = document.createElement("img");
+      let description = document.createElement("p");
+      let network = document.createElement("p");
+      let runtime = document.createElement("p");
+      const rawScore = result.score * 100;
+      const score = rawScore.toFixed(1);
+
+      movieTitle.innerText = `Title: ${result.show.name}`;
+      movieRating.textContent = `Rating: ${score}`;
+      // console.log(movieRating);
+
+      newImg.src = result.show.image.medium;
+      originalImg = result.show.image.original;
+      description.innerHTML = `${result.show.summary}`;
+      runtime.textContent = `Runtime: ${result.show.runtime} minutes`;
+
+      movieDiv.append(movieSpan);
+      movieSpan.append(newImg, movieTitle);
+      movieTitle.append(movieRating);
+
+      newImg.addEventListener("click", (e) =>
+        handleSelectMovie(e, movieSpan, originalImg, description, runtime)
+      );
+    }
+  }
+};
+
+//**Handlers */
+function handleSelectMovie(e, movieSpan, originalImg, description, runtime) {
+  const commentForm = document.createElement("form");
+  const showSearch = document.querySelector("#show-search");
+  let image = document.querySelector("img");
+  const likes = document.createElement("span");
+
+  movieDiv.innerHTML = "";
+
+  movieDiv.append(movieSpan);
+  movieSpan.append(description, runtime);
+
+  image.src = originalImg;
+
+  likes.id = "likes";
+  likes.innerHTML = `  
+      <div class="likes-section">
+      <span id="like-count" class="likes">${0} likes</span>
+      <button type="click" id="like-button" class="like-button">♥</button>
+      </div>`;
+
+  commentForm.id = "comment-form";
+  commentForm.innerHTML = `
+      <h4>Comments:</h4>
+      <ul id="comment-list"></ul>
+      <input name="newComment" type="text" placeholder="Add comment"/>
+      <button type="submit" id="comment-btn"/>Add Comment</button>
+   `;
+
+  likesDiv.append(likes);
+  commentDiv.append(commentForm);
+
+  likeBtn = document.querySelector("#like-button");
+
+  likeBtn.addEventListener("click", handleLikes);
+  commentForm.addEventListener("submit", handleNewComment);
+}
+
+function handleLikes() {
+  console.log("test");
+  const likes = document.querySelector("#like-count");
+  let count = Number(likes.textContent.split(" ")[0]);
+  count++;
+  likes.innerText = count + " likes";
+}
+
+function handleNewComment(e) {
+  e.preventDefault();
+  // console.log(e);
+
+  const commentUl = document.querySelector("#comment-list");
+  const commentLi = document.createElement("li");
+
+  commentLi.textContent = e.target.newComment.value;
+
+  commentUl.appendChild(commentLi);
+}
+
+// likesBtn.addEventListener("click", incrementLikes);
+
+// function incrementLikes() {
+//   let count = Number(likes.textContent.split(" ")[0]);
+//   count++;
+//   likes.innerText = count + " likes";
+// }
